a841672ef04885f35ff63656f9ea0b2e5195d812
Ghislain Vieilledent <ghislain.vieilledent@cirad.fr>
2022-11-02 19:03:30 +1100

    Use of file.path for paths

--- R/merge_files.R
+++ R/merge_files.R
@@ -1,14 +1,14 @@
-merge_files <- function(environ_path, climat_path, destination){
+merge_files <- function(environ_path, climate_path, destination){
   #' Create a multilayer Tiff file with environ & climat variables.
   #'
   #' @description
   #' Use output of `get_env_variables` and `get_chelsa_current` to create an unique file with more than 100 variables.
   #' Can be use with files created by `get_chelsa_futur`.
   #'
-  #' @param environ_path character. absolute path to environ.tif file, output of `get_env_variables`.
-  #' @param climat_path  character. absolute path to current_chelsa.tif.tif file, output of `get_chelsa_variables`
-  #' @param destination character. absolute path where to download files like `here()` output.
-  #' @return character. absolute path to gecevar.tif.
+  #' @param environ_path character. Absolute path to `environ.tif` file, output of `get_env_variables`.
+  #' @param climate_path  character. Absolute path to `current_chelsa.tif` file, output of `get_chelsa_variables`
+  #' @param destination character. Absolute path for the output directory.
+  #' @return character. Absolute path to gecevar.tif.
   #'
   #' @details
   #'
@@ -23,12 +23,12 @@ merge_files <- function(environ_path, climat_path, destination){
   nodat = -32768
   system(glue('gdal_merge.py -ot Int16 -of GTiff -o {paste(destination, "gecevar_noname.tif", sep = "/")} -a_nodata {nodat} -separate \\
             -co "COMPRESS=LZW" -co "PREDICTOR=2" {environ_path} {climat_path}'), ignore.stdout = TRUE, ignore.stderr = TRUE)
-  all_var <- rast(paste(destination,  "gecevar_noname.tif", sep = "/"))
-  names(all_var) <-  c(names(rast(environ_path)), names(rast(climat_path)))
-  writeRaster(x = all_var, filename = paste(destination, "gecevar.tif", sep = "/"), overwrite = TRUE)
+  all_var <- rast(file.path(destination,  "gecevar_noname.tif"))
+  names(all_var) <-  c(names(rast(environ_path)), names(rast(climate_path)))
+  writeRaster(x = all_var, filename = file.path(destination, "gecevar.tif"), overwrite = TRUE)
 
-  unique_values <- unique(values(rast(paste(destination, "gecevar.tif", sep = "/"))[[6]]))
+  unique_values <- unique(values(rast(file.path(destination, "gecevar.tif"))[[6]]))
   create_xml_legend(unique_values = unique_values, destination = destination, name_file = "gecevar")
-  unlink(paste(destination, "gecevar_noname.tif", sep = "/"))
+  unlink(file.path(destination, "gecevar_noname.tif"))
 
   }
